1c7a5973bdbe98856a33eafc25185d09cb2daa46
el0omar <elbaoomar@gmail.com>
2023-05-26 14:18:18 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,9 @@
  * is written by mbah
  * Return: 0
  */
+
 int main(void)
 {
-_putchar('_');
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -18,3 +18,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
+
