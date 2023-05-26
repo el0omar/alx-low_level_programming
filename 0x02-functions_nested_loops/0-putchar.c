c85ae6fa3d3fe54a0b31e073df307e34ef6d7a79
el0omar <elbaoomar@gmail.com>
2023-05-26 11:02:38 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	_putchar('_')
+	_putchar('_');
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
+        return (0);
 }
